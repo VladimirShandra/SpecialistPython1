1b88d36341230eb945589fb3f1cd77ff134c3c5a
VladimirShandra <87034414+VladimirShandra@users.noreply.github.com>
2021-07-12 10:45:54 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,10 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+numbers = [2,-5,8,9,-25,25,4]
+results = []
+for el in numbers:
+    if el > 0 and el**0.5%1==0:
+        results.append(int(el**0.5))
+print(results)
