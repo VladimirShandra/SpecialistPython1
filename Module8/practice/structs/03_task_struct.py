48aba568572a60b09848b018a0cadbd9514a22da
VladimirShandra <87034414+VladimirShandra@users.noreply.github.com>
2021-07-15 20:24:48 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,26 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+
+
+from random import randint
+
+n = randint(0, 15)
+random_list = [randint(-100, 100) for i in range(n)]
+
+
+def num_counts(number_list):
+    count = 0
+    for el in number_list:
+        if el < 10:
+            count += 1
+    return count
+
+count_list = [el for el in random_list if el < 10]
+sum_list = [el for el in random_list if el > 0]
+average_list = [el for el in random_list if el % 2 == 0]
+print("Список произвольных чисел:", random_list)
+print("Количество элементов списка не превышающие 10:", num_counts(count_list))
+print("Сумма всех положительных элементов списка:",sum(sum_list))
+print("Список четных элементов:", average_list)
+print("Среднее арифметическое всех четных элементов:",sum(average_list) / len(average_list))
