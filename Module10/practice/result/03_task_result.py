717bf0c91ea94f725e3128c9e746e7cf1276cdac
VladimirShandra <87034414+VladimirShandra@users.noreply.github.com>
2021-07-18 01:35:13 +0300

    Update 03_task_result.py

--- Module10/practice/result/03_task_result.py
+++ Module10/practice/result/03_task_result.py
@@ -3,4 +3,17 @@
 # Формат выходных данных: Выведите целое число недель.
 # Уточнение: каждые 7 прошедших дней равны одной полной неделе.
 
+def ending_weeks(number_weeks):
+    if number_weeks % 10 == 1 and number_weeks != 11:
+        return "неделя"
+    elif 2 <= number_weeks % 10 <= 4 and number_weeks // 10 != 1:
+        return "недели"
+    else:
+        return "недель"
 
+
+number_of_days = int(input("Введите количество прошедших дней:"))
+
+number_weeks = number_of_days // 7
+
+print("С некоторого момнета прошло :", number_weeks, ending_weeks(number_weeks))
