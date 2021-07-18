27f3bfefe373c0121f92f7692bc45877e3a01c48
VladimirShandra <87034414+VladimirShandra@users.noreply.github.com>
2021-07-18 14:31:07 +0300

    Update 09_task_result.py

--- Module10/practice/result/09_task_result.py
+++ Module10/practice/result/09_task_result.py
@@ -1,3 +1,28 @@
 # Напишите функцию принимающую на вход целое числом n. И возвращающую список из n элементов.
 # заполненный случайными целыми числами в диапазоне от a до b.
 # Примечание: для генерации случайного числа используйте import random
+
+def list_n_numbers(num_of_el, of, to):
+    import random
+    list_numbers = []
+    for _ in range(num_of_el):
+        list_numbers.append(random.randint(of, to))
+    return list_numbers
+
+
+number_of_elements = int(input("Укажите параметры списка.\nВведите количество элементов списка:"))
+
+while True:                                 # Идет проверка на корректность указания диапазона
+    print("Элементы будут в диапазоне:")
+    a = int(input("от a:"))
+    b = int(input("до b:"))
+    try:
+        if a > b:
+            raise ValueError
+        break
+    except ValueError:
+        print("Некорректно введен диапазон, a должно быть меньше b. Введите новые параметры")
+
+print("Список состоит из следующих элементов:", *list_n_numbers(number_of_elements, a, b))
+
+
